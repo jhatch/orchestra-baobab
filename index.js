6f9691009ccd58f3ad7bca6ebe0c819de3e5dbcd
John Hatcher <jhatcher@jibe.com>
2015-09-05 17:28:44 -0400

    adding a search button class

--- index.js
+++ index.js
@@ -3,9 +3,19 @@
 // the main controller
 var SearchInput  = require('./lib/SearchInput');
 var ResultsGrid  = require('./lib/ResultsGrid');
+var bing         = require('./lib/bing');
 
-var mySearchInput = new SearchInput('.search-control');
-var myResultsGrid = new ResultsGrid('.search-results');
+var mySearchInput  = new SearchInput('.search-control');
+
+var mySearchButton = new Button('.search-button', 'Search!', function () {
+  // take current state, and pass to results grid to refresh
+  
+});
+
+var myResultsGrid  = new ResultsGrid('.search-results', function (done) {
+  bing.search('test', done);
+});
+
 
 mySearchInput.render();
 myResultsGrid.render();
