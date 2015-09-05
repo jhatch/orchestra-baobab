c98cd440d8d3ed0bbb1a5f297e224b22733fb0b8
John Hatcher <jhatcher@jibe.com>
2015-09-05 17:37:56 -0400

    still not even bothering to store state, but the 3 components are now hooked into each other

--- index.js
+++ index.js
@@ -3,20 +3,20 @@
 // the main controller
 var SearchInput  = require('./lib/SearchInput');
 var ResultsGrid  = require('./lib/ResultsGrid');
+var Button       = require('./lib/Button');
 var bing         = require('./lib/bing');
 
 var mySearchInput  = new SearchInput('.search-control');
-
-var mySearchButton = new Button('.search-button', 'Search!', function () {
-  // take current state, and pass to results grid to refresh
-  
-});
 
 var myResultsGrid  = new ResultsGrid('.search-results', function (done) {
   bing.search('test', done);
 });
 
+var mySearchButton = new Button('.search-button', 'Search!', function () {
+  alert('a');
+  myResultsGrid.render();
+});
 
 mySearchInput.render();
-myResultsGrid.render();
+mySearchButton.render();
 
