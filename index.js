746a20fa4dbc66678c756221feef0fc5587eeba1
John Hatcher <jhatcher@jibe.com>
2015-09-05 14:23:21 -0400

    basic implementation of the search input control

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict'
 
 // the main controller
-var StateManager = require('./lib/StateManager');
 var SearchInput  = require('./lib/SearchInput');
 var ResultsGrid  = require('./lib/ResultsGrid');
 
 var mySearchInput = new SearchInput('.search-control');
 var myResultsGrid = new ResultsGrid('.search-results');
 
- 
+mySearchInput.render();
