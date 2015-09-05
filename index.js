d8de9baf7ccd65f629754ec3a6f3e22e06673167
John Hatcher <jhatcher@jibe.com>
2015-09-05 17:47:51 -0400

    starting to manage internal state

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 'use strict'
 
 // the main controller
-var SearchInput  = require('./lib/SearchInput');
-var ResultsGrid  = require('./lib/ResultsGrid');
-var Button       = require('./lib/Button');
-var bing         = require('./lib/bing');
+var Baobab      = require('baobab');
+var SearchInput = require('./lib/SearchInput');
+var ResultsGrid = require('./lib/ResultsGrid');
+var Button      = require('./lib/Button');
+var bing        = require('./lib/bing');
+
+var state = new Baobab({
+  searchQuery: {
+    term: ''
+  }
+});
+
+var query = state.select('searchQuery');
 
 var mySearchInput  = new SearchInput('.search-control');
 
-var myResultsGrid  = new ResultsGrid('.search-results', function (done) {
-  bing.search('test', done);
+var myResultsGrid  = new ResultsGrid('.search-results', function fetchResults(done) {
+  bing.search(query.select('term').get(), done);
 });
 
 var mySearchButton = new Button('.search-button', 'Search!', function () {
-  alert('a');
+  query.set('term', mySearchInput.get());
   myResultsGrid.render();
 });
 
