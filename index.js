dcac0ef29dd3bd286e4f805a357ba748734c9a49
John Hatcher <jhatcher@jibe.com>
2015-09-06 03:44:31 -0400

    basic implementation is up and working

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 // the main controller
 var Baobab      = require('baobab');
+var url         = require('url');
+var history     = require('html5-history-api');
+var $           = require('jquery');
 var SearchInput = require('./lib/SearchInput');
 var ResultsGrid = require('./lib/ResultsGrid');
 var Button      = require('./lib/Button');
@@ -15,6 +18,11 @@ var state = new Baobab({
 
 var query = state.select('searchQuery');
 
+// keep the URL up-to-date
+query.on('update', function () {
+  history.pushState(null, null, '?' + $.param(query.get()));
+});
+
 var mySearchInput  = new SearchInput('.search-control');
 
 var myResultsGrid  = new ResultsGrid('.search-results', function fetchResults(done) {
@@ -23,6 +31,7 @@ var myResultsGrid  = new ResultsGrid('.search-results', function fetchResults(do
 
 var mySearchButton = new Button('.search-button', 'Search!', function () {
   query.set('term', mySearchInput.get());
+  state.commit();
   myResultsGrid.render();
 });
 
